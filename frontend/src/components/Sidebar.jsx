4782bddec10c5c50e7e4594719745aab91ba451e
Nhật Trường <nguyenthanhuyen3040@gmail.com>
2025-10-14 15:25:32 +0700

    Update again about Login

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,8 +4,8 @@ export default function Sidebar({ onFilter }) {
   return (
     <div className="sidebar">
       <button onClick={() => onFilter("all")}>TẤT CẢ</button>
-      <button onClick={() => onFilter("coffee")}>COFFEE</button>
-      <button onClick={() => onFilter("tea")}>TRÀ</button>
+      <button onClick={() => onFilter("Cà phê")}>Cà phê</button>
+      <button onClick={() => onFilter("Trà")}>Trà</button>
     </div>
   );
 }
